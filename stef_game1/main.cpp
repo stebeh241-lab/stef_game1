d1d3c2d501b5b39d49f67c4b97c8d98282682096
Stefan Bergman <stebeh241@gmail.com>
2026-02-06 22:52:45 +0100

    o

--- stef_game1/main.cpp
+++ stef_game1/main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <raylib.h>
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) {
+  int WIDTH = 1600;
+  int HEIGHT = 900;
+  int ballx = WIDTH / 2;
+  int bally = HEIGHT / 2;
 
-{
-  InitWindow(1600, 900, "le gamey");
+  InitWindow(WIDTH, HEIGHT, "le gamey");
   SetTargetFPS(60);
 
   // Game loop
   while (WindowShouldClose() == false) {
     // 1. Event hadling
-
+    if (IsKeyDown(KEY_W)) {
+      bally -= 2;
+    } else if (IsKeyDown(KEY_S)) {
+      bally += 2;
+    } else if (IsKeyDown(KEY_A)) {
+      ballx -= 2;
+    } else if (IsKeyDown(KEY_D)) {
+      ballx += 2;
+    }
     // 2. Update positions
 
     // 3. Drawing
     BeginDrawing();
+    ClearBackground(BLACK);
+    DrawCircle(ballx, bally, 50, RED);
     EndDrawing();
   }
 
